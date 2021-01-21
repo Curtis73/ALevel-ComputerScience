c6d0a0cb07e6c2371954e8e7d0f2107df5d431e7
Curtis73 <mousiearmyinfo@gmail.com>
2021-01-21 11:22:15 +0000

    Add programs from 21/01/2021 lesson

---
+++ Notebook.py
@@ -0,0 +1,13 @@
+# Notebook Program
+# 21/01/2021
+
+notes = ["","","","","","","","","",""]
+
+while True:
+    for x in range (len(notes)):
+        print(x,":",notes[x])
+    num = int(input("Please enter a note to change: "))
+
+    if (num >= 0 and num < 10):
+        new_note = input("Please enter the new note: ")
+        notes[num] = new_note
---
+++ RPG.py
@@ -0,0 +1,46 @@
+# RPG Inventory Program
+# 21/01/2021
+
+import random
+
+inventory = []
+
+def pick(item):
+    inventory.append(item)
+    print(item, "has been added to your inventory!")
+
+def drop(item):
+    if item in inventory:
+        inventory.remove(item)
+        print (item, "has been removed from your inventory!")
+    else:
+        print (item, "couldn't be found!")
+
+def pull():
+    print (random.choice(inventory), "has been pulled!")
+
+def search():
+    for item in inventory:
+        print (item)
+
+# Main Program
+while True:
+    command = input("Please select 'pick', 'drop', 'pull' or search")
+
+    if command == "pick":
+        item = input("Please enter an item to add to your inventory")
+        pick(item)
+    elif command == "drop":
+        item = input("Please enter an item to remove from your inventory")
+        drop(item)
+    elif command == "pull":
+        pull()
+    elif command == "search":
+        search()
+    else:
+        print ("Invalid command, please try again")
+        print(command)
+
+
+
+
